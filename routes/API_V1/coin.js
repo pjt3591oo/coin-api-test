70cb90576f24b66e506d1d278bcdaae20a538565
pjt3591oo <pjt3591oo@gmail.com>
2018-10-18 13:12:06 +0900

    CREATED project

---
+++ routes/API_V1/coin.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/:currency', (req, res, next) => {
+
+})
+
+module.exports = router;
---
+++ routes/API_V1/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var coin = require('./coin')
+
+var router = express.Router();
+
+router.use('/data/currency', coin)
+module.exports = router;
